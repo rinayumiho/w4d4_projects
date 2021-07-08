22e2dbe10099c1e2c3d0dffda424de0d2a8e22b4
Winnie <winnieng3210@gmail.com>
2021-07-08 14:56:00 -0400

    spec switch

--- first_tdd_projects/lib/code.rb
+++ first_tdd_projects/lib/code.rb
@@ -1,3 +1,21 @@
-def my_uniq(arr)
-    arr.uniq
+class Array
+    
+    def my_uniq
+       arr = []
+       self.each do |ele|
+        arr << ele if !arr.include?(ele)
+       end
+       arr
+    end
+
+    def two_sum 
+        array = []
+        (0...self.length).each do |i|
+            ((i+1)...self.length).each do |j|
+                array << [i,j] if self[i] + self[j] == 0
+            end
+        end
+        array
+    end
+
 end
--- first_tdd_projects/spec/code_spec.rb
+++ first_tdd_projects/spec/code_spec.rb
@@ -1,15 +1,24 @@
 require 'code'
 
-describe 'Code' do 
-    describe 'my_uniq' do
-        it "should accept an array of numbers as argument" do
-            my_uniq([1, 2, 1, 3, 3])
-        end
-
+describe 'Array' do 
+    describe '#my_uniq' do
         it "should return unique elements in the order they first appear" do 
-            expect(my_uniq([1, 2, 1, 3, 3])).to eq([1, 2, 3])
-            expect(my_uniq([])).to eq([])
+            expect([1, 2, 1, 3, 3].my_uniq).to eq([1, 2, 3])
+            expect([].my_uniq).to eq([])
         end
     end
+
+    describe "#two_sum" do 
+        it "should return all pairs of positions that sum to zero" do
+            expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+            expect([].two_sum).to eq([])
+            expect([-1, -1, 1, 1].two_sum).to eq([[0,2], [0,3], [1,2], [1,3]])
+        end
+
+        it "should be sorted smaller index before bigger index" do
+            expect([-1, 0, 2, -2, 1].two_sum).not_to eq([[2, 3],[0, 4]])
+        end        
+    end
+
 
 end
